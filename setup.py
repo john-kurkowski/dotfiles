571b6fec2d766c84bbe569b60f9c47be738584b7
John Kurkowski <john.kurkowski@gmail.com>
2015-09-19 12:23:33 -0700

    Apply pylint recommendations to setup.py

--- setup.py
+++ setup.py
@@ -7,35 +7,25 @@ import errno
 import glob
 import os
 import re
-import shlex
 import shutil
 import subprocess
 
 MOD_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
-def call(cmd):
-    split_cmd = shlex.split(cmd)
-    p = subprocess.Popen(split_cmd, stdout=subprocess.PIPE)
-    stdout, _ = p.communicate()
-    if p.returncode:
-        raise subprocess.CalledProcessError(p.returncode, split_cmd)
-    return stdout
-
-
 def dotdirs():
     '''The unique custom dirs tracked by this repo. Ignores intermediate dirs
-    if they don't contain any tracked files.'''
+    that don't contain tracked files.'''
     def extract_dir(path):
         spl = path.rsplit(os.sep, 1)
         is_dir = len(spl) > 1
         return spl[0] if is_dir else None
 
-    return set(filter(bool, (extract_dir(d) for d in dotfiles())))
+    return set(dotdir for dotdir in (extract_dir(d) for d in dotfiles()) if dotdir)
 
 
 def dotfiles():
-    return call('git ls-files .*').splitlines()
+    return subprocess.check_output('git ls-files .*', shell=True).splitlines()
 
 
 def mkdir_p(path):
@@ -46,63 +36,89 @@ def mkdir_p(path):
             raise
 
 
-def install(args):
-    dest = os.path.expanduser('~/')
-
-    for f in dotdirs():
-        dest_dotdir = os.path.join(dest, f)
+def clear_tracked(dest):
+    '''Clear files and dirs tracked by this repo, in the given destination dir.
+    Does not clear untracked intermediate dirs or untracked top-level
+    dotfiles.'''
+    for dotfile in dotdirs():
+        dest_dotdir = os.path.join(dest, dotfile)
         try:
             shutil.rmtree(dest_dotdir)
         except OSError as exc:
             if exc.errno != errno.ENOENT:
                 raise
 
-    for f in dotfiles():
-        dest_dotfile = os.path.join(dest, f)
+
+def copy_dotfiles(dest):
+    for dotfile in dotfiles():
+        dest_dotfile = os.path.join(dest, dotfile)
         dest_dotfile_dir = os.path.dirname(dest_dotfile)
         mkdir_p(dest_dotfile_dir)
 
-        src = os.path.join(MOD_DIR, f)
+        src = os.path.join(MOD_DIR, dotfile)
         is_empty_dir = os.path.isdir(src) and not os.listdir(src)
         if not is_empty_dir:
             shutil.copy2(src, dest_dotfile)
 
+
+def copy_host_specific_dotfiles(dest, host_specific_settings):
     hostspecific = os.path.join(dest, '.hostspecific')
-    if args.settings:
-        settings = os.path.join(MOD_DIR, 'settings', args.settings)
+    if host_specific_settings:
+        settings = os.path.join(MOD_DIR, 'settings', host_specific_settings)
         shutil.copy2(settings, hostspecific)
     else:
-        with open(hostspecific, 'w') as f:
+        with open(hostspecific, 'w') as _:
             pass
 
+
+def install_user(dest, email):
+    '''Set the email for Git commits on the system.'''
     gitrc = os.path.join(dest, '.gitconfig')
     old_email = re.escape('john.kurkowski@gmail.com')
-    new_email = re.escape(args.email)
+    new_email = re.escape(email)
     email_cmd = """perl -pi -e 's/%s/%s/g' %s""" % (old_email, new_email, gitrc)
-    call(email_cmd)
+    subprocess.check_call(email_cmd, shell=True)
 
 
-def export(args):
+def install(args):
+    '''Install dotfiles into home dir.'''
+    dest = os.path.expanduser('~/')
+    clear_tracked(dest)
+    copy_dotfiles(dest)
+    copy_host_specific_dotfiles(dest, args.settings)
+    install_user(dest, args.email)
+
+
+def export(dummy_args):
     """The inverse of install."""
     src = os.path.expanduser('~/')
-    for f in dotfiles():
-        is_empty_dir = os.path.isdir(f) and not os.listdir(f)
+    for dotfile in dotfiles():
+        is_empty_dir = os.path.isdir(dotfile) and not os.listdir(dotfile)
         if not is_empty_dir:
-            shutil.copy2(os.path.join(src, f), os.path.join(MOD_DIR, f))
+            shutil.copy2(os.path.join(src, dotfile), os.path.join(MOD_DIR, dotfile))
 
-if __name__ == "__main__":
-    settings_choices = [os.path.basename(f) for f in glob.glob(os.path.join(MOD_DIR, 'settings', '[A-Za-z]*'))]
+
+def main():
+    '''Parse args and execute their subcommand.'''
+    setting_paths = glob.glob(os.path.join(MOD_DIR, 'settings', '[A-Za-z]*'))
+    settings_choices = [os.path.basename(p) for p in setting_paths]
 
     parser = argparse.ArgumentParser(description="Install dotfiles")
     subparsers = parser.add_subparsers(title='subcommands')
 
     install_cmd = subparsers.add_parser('install')
     install_cmd.set_defaults(func=install)
-    install_cmd.add_argument('--email', '-e', default='john.kurkowski@gmail.com', help='Git author email address (default: personal)')
-    install_cmd.add_argument('settings', choices=settings_choices, help='Install a predefined suite of settings.')
+    install_cmd.add_argument('--email', '-e', default='john.kurkowski@gmail.com',
+                             help='Git author email address (default: personal)')
+    install_cmd.add_argument('settings', choices=settings_choices,
+                             help='Install a predefined suite of settings.')
 
     export_cmd = subparsers.add_parser('export')
     export_cmd.set_defaults(func=export)
 
     args = parser.parse_args()
     args.func(args)
+
+
+if __name__ == "__main__":
+    main()
