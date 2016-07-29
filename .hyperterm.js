f2466d3ca80e64f5fe029414e4d07b94ab7dc4ba
John Kurkowski <john.kurkowski@gmail.com>
2016-07-28 21:05:23 -0700

    Apply snazzier colors in HyperTerm

--- .hyperterm.js
+++ .hyperterm.js
@@ -65,7 +65,9 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: [
+    'hyperterm-snazzy',
+  ],
 
   // in development, you can create a directory under
   // `~/.hyperterm_plugins/local/` and include it here
