15274a01da5423749794eb528498915f6a527202
John Kurkowski <john.kurkowski@gmail.com>
2018-01-27 12:14:04 -0800

    Apply .eslintrc.js to itself

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,33 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true,
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'sourceType': 'module',
+  },
+  'rules': {
+    'comma-dangle': [
+      'error',
+      'always-multiline',
+    ],
+    'indent': [
+      'error',
+      2,
+    ],
+    'linebreak-style': [
+      'error',
+      'unix',
+    ],
+    'quotes': [
+      'error',
+      'single',
+    ],
+    'semi': [
+      'error',
+      'always',
+    ],
+  },
 };
