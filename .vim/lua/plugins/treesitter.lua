49d04a870a5e41061f618e41d432ef4003e89b10
John Kurkowski <john.kurkowski@gmail.com>
2025-12-30 14:06:19 -0800

    Skip nvim-treesitter fold instruction
    I don't tend to use folds, and these lines from their README seemingly fold everything by default.

--- .vim/lua/plugins/treesitter.lua
+++ .vim/lua/plugins/treesitter.lua
@@ -63,8 +63,6 @@ return {
             )
           then
             vim.treesitter.start(args.buf)
-            vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-            vim.wo[0][0].foldmethod = 'expr'
             vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
           end
         end,
