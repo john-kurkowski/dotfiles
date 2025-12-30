fc2d1157a3e5f40c75ea7bf1f136be21a4601bda
John Kurkowski <john.kurkowski@gmail.com>
2025-12-30 13:19:18 -0800

    Upgrade nvim-treesitter breaking version
    * Loop over installed languages to emulate prior, automatic setup
      * `ensure_installed`, highlight, and indent
      * H/T https://github.com/sharpchen/nix-config/blob/d357b8026930b9ec380e8cd480bbf983ed984cb0/dotfiles/nvim-config/lua/plugins/treesitter.lua#L55-L66
    * Require extracted, own plugins, rather than prior bundling
      * nvim-treesitter-textobjects
      * Remove vim-matchup config; it automatically configures in the
        presence of treesitter
    * Switch to built-in Just parser

--- .vim/lua/plugins/treesitter.lua
+++ .vim/lua/plugins/treesitter.lua
@@ -1,59 +1,92 @@
+function table.except(super, sub)
+  local result = {}
+  local seenInResult = {}
+  local lookupSub = {}
+
+  for _, value in ipairs(sub) do
+    lookupSub[value] = true
+  end
+
+  for _, value in ipairs(super) do
+    if not lookupSub[value] and not seenInResult[value] then
+      table.insert(result, value)
+      seenInResult[value] = true
+    end
+  end
+
+  return result
+end
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    -- Pin to legacy master branch.
-    -- TODO: upgrade to breaking main branch.
-    branch = "master",
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
-    dependencies = {
-      "andymass/vim-matchup", -- for matchup integration
-    },
+    lazy = false,
     config = function()
-      require('nvim-treesitter.configs').setup({
-        ensure_installed = {
-          "astro",
-          "bash",
-          "css",
-          "glimmer",
-          "html",
-          "javascript",
-          "json",
-          "json5",
-          "just",
-          "make",
-          "markdown",
-          "python",
-          "ruby",
-          "rust",
-          "scss",
-          "svelte",
-          "swift",
-          "toml",
-          "tsx",
-          "typescript",
-          "vue",
-          "yaml",
-        },
-        highlight = {
-          enable = true,
-        },
-        incremental_selection = { enable = true },
-        indent = { enable = true },
-        matchup = { enable = true },
-        textobjects = { enable = true },
-      })
+      local treesitter = require('nvim-treesitter')
+      treesitter.setup {}
 
-      -- Custom parser for justfiles
-      require("nvim-treesitter.parsers").get_parser_configs().just = {
-        install_info = {
-          url = "https://github.com/IndianBoy42/tree-sitter-just",
-          files = { "src/parser.c", "src/scanner.cc" },
-          branch = "main",
-          use_makefile = true,
-        },
-        maintainers = { "@IndianBoy42" },
+      local should_install = {
+        "astro",
+        "bash",
+        "css",
+        "glimmer",
+        "html",
+        "javascript",
+        "json",
+        "json5",
+        "just",
+        "make",
+        "markdown",
+        "python",
+        "ruby",
+        "rust",
+        "scss",
+        "svelte",
+        "swift",
+        "toml",
+        "tsx",
+        "typescript",
+        "vue",
+        "yaml",
       }
+
+      treesitter.install(table.except(should_install, treesitter.get_installed()))
+
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(args)
+          if
+            vim.list_contains(
+              treesitter.get_installed(),
+              vim.treesitter.language.get_lang(args.match)
+            )
+          then
+            vim.treesitter.start(args.buf)
+            vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+            vim.wo[0][0].foldmethod = 'expr'
+            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    init = function()
+      -- Disable entire built-in ftplugin mappings to avoid conflicts.
+      -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
+      vim.g.no_plugin_maps = true
+
+      -- Or, disable per filetype (add as you like)
+      -- vim.g.no_python_maps = true
+      -- vim.g.no_ruby_maps = true
+      -- vim.g.no_rust_maps = true
+      -- vim.g.no_go_maps = true
+    end,
+    config = function()
+      -- put your config here
     end,
   },
 }
