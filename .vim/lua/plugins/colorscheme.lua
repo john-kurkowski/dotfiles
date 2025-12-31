f18c14877760bc210cfdf23c1465483394028aef
John Kurkowski <john.kurkowski@gmail.com>
2025-12-30 22:26:56 -0800

    Colocate colorscheme config with plugin

--- .vim/lua/plugins/colorscheme.lua
+++ .vim/lua/plugins/colorscheme.lua
@@ -3,5 +3,11 @@ return {
     "cocopon/iceberg.vim",
     lazy = false,
     priority = 1000,
+    init = function()
+      vim.opt.background = 'dark'
+      vim.opt.cursorline = true
+      vim.opt.cursorlineopt = 'number'
+      vim.cmd.colorscheme('iceberg')
+    end,
   },
 }
