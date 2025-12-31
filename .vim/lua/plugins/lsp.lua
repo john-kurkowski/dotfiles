c5e9ed8126d14a9c5b9f08577ab374eb269da19c
John Kurkowski <john.kurkowski@gmail.com>
2025-12-30 18:06:46 -0800

    Remove redundant diagnostics keymaps
    <C-]> already existed for jump to definition. Neovim 0.11 introduced default keymaps for the rest of these. https://neovim.io/doc/user/lsp.html#lsp-defaults

--- .vim/lua/plugins/lsp.lua
+++ .vim/lua/plugins/lsp.lua
@@ -11,32 +11,6 @@ return {
       vim.lsp.enable('ruff')
       vim.lsp.enable('ts_ls')
       vim.lsp.enable('vue_ls')
-
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('my.lsp', {}),
-        callback = function(args)
-          local opts = { buffer = args.buf }
-
-          -- Navigation
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-
-          -- Documentation
-          vim.keymap.set('n', 'gk', vim.lsp.buf.hover, opts)
-          vim.keymap.set('n', 'gK', vim.lsp.buf.signature_help, opts)
-
-          -- Refactoring
-          vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-          vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
-
-          -- Diagnostics
-          vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-          vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-          vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
-        end,
-      })
     end,
   },
 }
