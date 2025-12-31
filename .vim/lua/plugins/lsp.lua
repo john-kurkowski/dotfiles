02f716c5704860b7b2e33be4697c5c759222128f
John Kurkowski <john.kurkowski@gmail.com>
2025-12-30 18:09:55 -0800

    Show virtual line diagnostics for current line

--- .vim/lua/plugins/lsp.lua
+++ .vim/lua/plugins/lsp.lua
@@ -3,6 +3,8 @@ return {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
+      vim.diagnostic.config({ virtual_lines = { current_line = true }, })
+
       vim.lsp.enable('astro')
       vim.lsp.enable('biome')
       vim.lsp.enable('eslint')
