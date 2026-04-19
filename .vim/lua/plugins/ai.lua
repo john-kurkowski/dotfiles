c46b10dee11e40ac89cbc35082f2eddafca6916a
John Kurkowski <john.kurkowski@gmail.com>
2026-04-18 20:50:14 -0700

    Remove Codeium
    I don't use AI autocomplete any more. These days, I'm yolo agents or caveman.

--- .vim/lua/plugins/ai.lua
+++ .vim/lua/plugins/ai.lua
@@ -1,16 +1,4 @@
 return {
-  -- AI code completion
-  {
-    "Exafunction/codeium.vim",
-    event = "InsertEnter",
-    init = function()
-      vim.g.codeium_no_map_tab = 1
-      vim.keymap.set("i", "<M-Tab>", function()
-        return vim.fn["codeium#Accept"]()
-      end, { expr = true, silent = true })
-    end,
-  },
-
   -- AI code chat
   {
     "olimorris/codecompanion.nvim",
